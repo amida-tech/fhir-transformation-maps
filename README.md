5dc93b8a5cbf8ae3dbf3dd2d25703359f94b651d
Matthew McCall <matt@mattmccall.com>
2019-02-04 10:43:00 -0800

    skeleton working.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fhir-transformation-maps
 FHIR transformation maps for use in converting between data formats (such as CDA>FHIR, etc.).
+
+https://github.com/bazaarvoice/jolt
+
--- src/main/java/com/amida/app/App.java
+++ src/main/java/com/amida/app/App.java
@@ -4,10 +4,29 @@ package com.amida.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import com.bazaarvoice.jolt.Chainr;
+import com.bazaarvoice.jolt.JsonUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+class JoltSample {
+
+    public static void main(String[] args) throws IOException {
+
+        // How to access the test artifacts, i.e. JSON files
+        //  JsonUtils.classpathToList : assumes you put the test artifacts in your class path
+        //  JsonUtils.filepathToList : you can use an absolute path to specify the files
+
+        ///
+
+        List chainrSpecJSON = JsonUtils.filepathToList( "/Users/matthew/Workspace/fhir-transformation-maps/data/spec.json" );
+        Chainr chainr = Chainr.fromSpec( chainrSpecJSON );
+
+        Object inputJSON = JsonUtils.filepathToList( "/Users/matthew/Workspace/fhir-transformation-maps/data/sample.json" );
+
+        Object transformedOutput = chainr.transform( inputJSON );
+        System.out.println( JsonUtils.toJsonString( transformedOutput ) );
     }
 }
